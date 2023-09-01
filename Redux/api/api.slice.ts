e1b482548a7332da86f1bd71ab617201fc20d93a
Ataur Rahman <rahman.sharif39n@gmail.com>
2023-09-02 02:18:42 +0600

    base url update in redux

--- Redux/api/api.slice.ts
+++ Redux/api/api.slice.ts
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const ApiSlice = createApi({
     reducerPath:'api',
     baseQuery:fetchBaseQuery({
-        baseUrl:'https://books-shop-backend-production.up.railway.app/api/v1',
+        baseUrl:'https://book-shop-tl35.onrender.com/api/v1',
     }),
     endpoints:()=>({})
 })
